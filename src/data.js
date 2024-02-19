0cb82790164d5f2aefb8119652f0130dc75b7afe
peter <peterlenard.arnoza.cics@ust.edu.ph>
2024-02-19 15:18:27 +0800

    data file

---
+++ src/data.js
@@ -0,0 +1,17 @@
+const productList = [
+  {
+    name: "Beats Studio Pro",
+    price: "349.99",
+    img: "./beatsstudiopro.png",
+  },
+  {
+    name: "Beats Studio Buds",
+    price: "149.99",
+    img: "./beatsstudiopro.png",
+  },
+  {
+    name: "Beats Solo Wireless",
+    price: "99.99",
+    img: "./beatsstudiopro.png",
+  },
+];
